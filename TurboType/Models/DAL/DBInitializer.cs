13e64b4b7f429870d2dde21d1ede4bb7a2207247
Burakovivan <burakovivan@gmail.com>
2015-12-27 01:04:16 +0200

    db initializer

--- TurboType/Models/DAL/DBInitializer.cs
+++ TurboType/Models/DAL/DBInitializer.cs
@@ -11,6 +11,7 @@ namespace TurboType.Models.DAL
         protected override void Seed(TTContext db)
         {
             db.Themes.Add(new Theme { ThemeId = 0, ThemeName = "tema" });
+            db.Stages.Add(new Stage { StageId = 0, Advice = "Тут совет", Complexity = 3, StageName = "Имя урока", TextToType = "йцу йцу йцууцй уц й ьялыф", ThemeId = 0 });
             db.SaveChanges();
             base.Seed(db);
         }
