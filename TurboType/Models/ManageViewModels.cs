b81c549de9d15456030836986077dc5e209ffe9f
Хикматова Диана <d.m.khikmatova@gmail.com>
2015-12-27 20:37:04 +0200

    Design-almost-all-pages

--- TurboType/Models/ManageViewModels.cs
+++ TurboType/Models/ManageViewModels.cs
@@ -43,17 +43,17 @@ namespace TurboType.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Старый пароль")]
         public string OldPassword { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Новый пароль")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
+        [Display(Name = "Повторите новый пароль")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
@@ -62,7 +62,7 @@ namespace TurboType.Models
     {
         [Required]
         [Phone]
-        [Display(Name = "Phone Number")]
+        [Display(Name = "Номер телефона")]
         public string Number { get; set; }
     }
 
