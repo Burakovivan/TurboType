f5143cbec106b46852e0dfbb22677b8fa81dde04
Хикматова Диана <d.m.khikmatova@gmail.com>
2015-12-28 09:22:28 +0200

    Change-db

--- TurboType/Models/DAL/DBInitializer.cs
+++ TurboType/Models/DAL/DBInitializer.cs
@@ -11,7 +11,12 @@ namespace TurboType.Models.DAL
         protected override void Seed(TTContext db)
         {
             db.Themes.Add(new Theme { ThemeId = 0, ThemeName = "tema" });
-            db.Stages.Add(new Stage { StageId = 0, Advice = "Тут совет", Complexity = 3, StageName = "Имя урока", TextToType = "ав вваава вавав вававав вавваав вавваав", ThemeId = 0, MaximalMistakes=2, MinimalSpeed=100 });
+            db.Stages.Add(new Stage { StageId = 1, Advice = "Совет:  используйте указательные пальцы для букв «а» и «о», средние – для «в» и «л»,  а для пробела – какой-то из больших. Обратите внимание, что на клавишах «а» и «о» есть специальные выступы для удобства определения правильного положения пальцев. ", Complexity = 1, StageName = "Урок 1", TextToType = "Аааа оооо вввв лллл вваа оолл ллоо аавв ва ол ав ло ваол лоав вал лава овал лов", ThemeId = 0, MaximalMistakes=4, MinimalSpeed=50 });
+            db.Stages.Add(new Stage { StageId = 2, Advice = "Совет: не забывайте использовать правила из предыдущего урока. Для букв «ы» и  «д» используйте безымянные пальцы, а для «ф» и «ж» - мизинцы. ", Complexity = 1, StageName = "Урок 2", TextToType = "ыыыы дддд ыывв ддлл вывы длдл фы фы жд жд ждал вода жало лыжа лафа", ThemeId = 0, MaximalMistakes = 3, MinimalSpeed = 50 });
+            db.Stages.Add(new Stage { StageId = 3, Advice = "Совет: для букв «п» и «р» используйте указательные пальцы. ", Complexity = 3, StageName = "Урок 3", TextToType = "Пппп рррр аппа роор вавалоло фыва ждло пора пара повар пар ров дыра жара плов выдра ", ThemeId = 0, MaximalMistakes = 2, MinimalSpeed = 100 });
+            db.Stages.Add(new Stage { StageId = 4, Advice = "Совет: для букв «у» и «к» используйте указательные пальцы,  а для «у» и «ш» - средние. ", Complexity = 3, StageName = "Урок 4", TextToType = "Укук шгшг куку гшгш кгкг шушу груша шарф кожа гора шок логово курага кора урок шов рывок рога шкаф фрак ", ThemeId = 0, MaximalMistakes = 2, MinimalSpeed = 100 });
+            db.Stages.Add(new Stage { StageId = 4, Advice = "Совет: для букв «м» и «ь» используйте указательные пальцы,  а для «с» и «б» - средние. ", Complexity = 3, StageName = "Урок 5", TextToType = "сс мм ьь бб мсмс бьбь дробь смог лото сова мост стол морковь голубь бор жолудь борсук форсаж", ThemeId = 0, MaximalMistakes = 2, MinimalSpeed = 100 });
+
             db.SaveChanges();
             base.Seed(db);
         }
